c4ed1ae7564f040a263fe58fbe765c0ceee6a706
Dev Manuels <emmanuel.umeile@gmail.com>
2023-02-16 11:34:04 -0800

    betty has problems sha

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -function entry
+ *
+ *
+ * print a string and return 0 then exits
+ * Return: int(0)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return (o);
+}
