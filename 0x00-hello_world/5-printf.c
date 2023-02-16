66b23faa593247ac26fa0a18c37fb6b98f45c646
Dev Manuels <emmanuel.umeile@gmail.com>
2023-02-16 11:45:13 -0800

    nextgit add .git add .git add .git add .git add .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -function entry
+ *
+ *
+ * print a string and return 0 then exits
+ * Return: int(0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
