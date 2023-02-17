47b70cf81ec2f320353caeaeea3b089a9ba6789e
Umeilechukwu Emmanuel <84356021+Dev-manuels@users.noreply.github.com>
2023-02-18 00:22:55 +0100

    Indentation error fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-        putchar('\n');
-    return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
 }
