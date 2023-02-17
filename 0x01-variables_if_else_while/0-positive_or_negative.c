a9353236cb942890ee22c37d3bd1d1dbbc1f7b58
Dev Manuels <emmanuel.umeile@gmail.com>
2023-02-17 12:41:56 -0800

    remove white space for betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
+# include <stdio.h>
 /**
  * main -main function entry
+ *
  * @n: number to be compared
- * @mes: String to be dislayed
  *
  * Return: (1);
  */
 int main(void)
 {
-  int n;
-  char *mes;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n > 0)
-    {
-      *mes = "is positive\n";
-      write(1, mes, strlen(mes));
-    }
-  else if (n == 0)
-    {
-      *mes = "is zero\n";
-      write(1, mes, strlen(mes));
-    }
-  else if (n < 0)
-    {
-      *mes = "is negative\n";
-      write(1, mes, strlen(mes));
-    }
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("is positive\n");
+	else if (n == 0)
+		printf("is zero\n");
+	else if (n < 0)
+		printf("is negative\n");
+	return (0);
 }
