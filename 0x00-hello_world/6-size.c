c03afac34d0308d72be2b92ffcabdad9d177d6ee
Dev Manuels <emmanuel.umeile@gmail.com>
2023-02-16 18:30:51 -0800

    Add spaces

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	float d;
+	char e;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu buyte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
+}
