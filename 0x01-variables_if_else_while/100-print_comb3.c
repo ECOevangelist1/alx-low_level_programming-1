c5a203defa4330e6228f08a8f0dfa8e465dfc845
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-18 03:16:43 +0100

    Betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int i,j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 		putchar((char) (i + 48));
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
 			putchar((char) (j + 48));
 			if (i == j)
