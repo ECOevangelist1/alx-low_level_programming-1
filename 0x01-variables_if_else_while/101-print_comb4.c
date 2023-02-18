4fc49dc4b2ade6460c420cb291ee27bc36824424
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-18 03:48:31 +0100

    Missed space

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,7 @@ int main(void)
 				putchar((char) (i + 48));
 				putchar((char) (j + 48));
 				putchar((char) (k + 48));
-				if (i == 7 && j == 8 && k =9)
+				if (i == 7 && j == 8 && k = 9)
 					continue;
 				putchar(',');
 				putchar(' ');
