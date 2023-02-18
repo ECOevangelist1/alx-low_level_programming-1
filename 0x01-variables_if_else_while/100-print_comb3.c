1b75f6c4804164e065726a3afd04b845eafe3950
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-02-18 03:08:23 +0100

    Advanced task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - main function entry
+*
+* Return: int(0)
+*/
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		putchar((char) (i + 48));
+		for (int j = 0; j < 10; j++)
+		{
+			putchar((char) (j + 48));
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(\n);
+}
